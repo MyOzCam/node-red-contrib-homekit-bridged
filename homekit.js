07ceb3709d560008d02fa597f835b324b472ebd3
MyOzCam <26063869+MyOzCam@users.noreply.github.com>
2019-04-09 09:28:26 +0800

    Do what I want

--- homekit.js
+++ homekit.js
@@ -2,10 +2,9 @@ module.exports = function(RED) {
     ("use strict");
 
     const HapNodeJS = require("hap-nodejs");
-    const Service = HapNodeJS.Service;
     const API = require("./lib/api.js")(RED);
     const HAPBridgeNode = require("./lib/HAPBridgeNode.js")(RED);
-    const HAPServiceNode = require("./lib/HAPServiceNode.js")(RED);
+    const HAPAccessoryNode = require("./lib/HAPAccessoryNode.js")(RED);
 
     // Initialize our storage system
     if (RED.settings.available()) {
@@ -17,7 +16,6 @@ module.exports = function(RED) {
 
     // Initialize API
     API.init();
-
     RED.nodes.registerType("homekit-bridge", HAPBridgeNode.init);
-    RED.nodes.registerType("homekit-service", HAPServiceNode.init);
+    RED.nodes.registerType("homekit-accessory", HAPAccessoryNode.init);
 };
