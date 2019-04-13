f75943d18db46de429e84b2804906159bb796f17
MyOzCam <26063869+MyOzCam@users.noreply.github.com>
2019-04-13 16:02:57 +0800

    cleaning up unwanted code (#3)
    * Update HAPServiceNode.js
    
    * Delete basic.js
    
    * Delete node-red-mock.js
    
    * Delete package-lock.json
    
    * Update homekit.js
    
    * Delete api.js
    
    * Update homekit.js
    
    * Update homekit.js
    
    * Update homekit.js
    
    * Update homekit.js
    
    * Update homekit.html
    
    * Delete _config.yml
    
    * Delete .travis.yml
    
    * Delete .gitignore
    
    * Delete bug_report.md
    
    * Delete feature_request.md
    
    * Update homekit.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update ServiceUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update ServiceUtils.js
    
    * Update ServiceUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update HAPServiceNode.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update ServiceUtils.js
    
    * Update homekit.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update HAPServiceNode.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update BridgeUtils.js
    
    * Delete ServerUtils.js
    
    * Update index.js
    
    * Update HAPBridgeNode.js
    
    * Update CharacteristicUtils.js
    
    * Update homekit.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update homekit.js
    
    * Update BridgeUtils.js
    
    * Update index.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update CharacteristicUtils.js
    
    * Update HAPServiceNode.js
    
    * Update ServiceUtils.js
    
    * Update HAPServiceNode.js
    
    * Create TV_accessory
    
    * Create .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Rename accessories/TV_accessory to accessories_sample/TV_accessory
    
    * Rename accessories_sample/TV_accessory to sample_accessories/samplesTV_accessory
    
    * Update HAPServiceNode.js
    
    * Update ServiceUtils.js

--- homekit.js
+++ homekit.js
@@ -1,23 +1,72 @@
 module.exports = function(RED) {
     ("use strict");
 
+    const debug = require("debug")("NRCHKB");
+    
     const HapNodeJS = require("hap-nodejs");
-    const Service = HapNodeJS.Service;
-    const API = require("./lib/api.js")(RED);
     const HAPBridgeNode = require("./lib/HAPBridgeNode.js")(RED);
     const HAPServiceNode = require("./lib/HAPServiceNode.js")(RED);
+    const Service = HapNodeJS.Service;
 
+    // Publish Accessory List
+    const publishAccessoryList = function () {
+        // Retrieve Accessory Types
+        RED.httpAdmin.get(
+            "/homekit/accessory/types",
+            RED.auth.needsPermission("homekit.read"),
+            function (req, res) {
+                res.json(HapNodeJS.Accessory.Categories);
+            }
+        );
+    };
+
+    // Publish Service List
+    const publishServiceList = function () {
+        // Retrieve Service Types
+        RED.httpAdmin.get(
+            "/homekit/service/types",
+            RED.auth.needsPermission("homekit.read"),
+            function (req, res) {
+                const data = {};
+                Object.keys(HapNodeJS.Service).forEach(function (key) {
+                    const val = HapNodeJS.Service[key];
+                    if (typeof val === "function" && val.hasOwnProperty("UUID")) {
+                        data[key] = val.UUID;
+                    }
+                });
+                res.json(data);
+            }
+        );
+    };
+    
     // Initialize our storage system
     if (RED.settings.available()) {
         const userDir = RED.settings.userDir;
         HapNodeJS.init(userDir + "/homekit-persist");
     } else {
         HapNodeJS.init();
-    }
-
+    };
+    
+    // Add padStart to nodejs 7
+    if (!String.prototype.padStart) {
+        String.prototype.padStart = function padStart(targetLength, padString) {
+            targetLength = targetLength >> 0; //truncate if number, or convert non-number to 0;
+            padString = String(typeof padString !== "undefined" ? padString : " ");
+            if (this.length >= targetLength) {
+                return String(this);
+            } else {
+                targetLength = targetLength - this.length;
+                if (targetLength > padString.length) {
+                    padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+                }
+                return padString.slice(0, targetLength) + String(this);
+            }
+        };
+    };
+    
     // Initialize API
-    API.init();
-
+    publishAccessoryList();
+    publishServiceList();
     RED.nodes.registerType("homekit-bridge", HAPBridgeNode.init);
     RED.nodes.registerType("homekit-service", HAPServiceNode.init);
-};
+}
